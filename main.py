5544aa462c6749745df4413c7e7e79a49ad2605b
Wojciech Wydmuch <wojciechwydmuch73@gmail.com>
2020-11-29 20:13:54 +0100

    Speech Recognition has been added

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import requests
 import json
+import pyttsx3
+
+import speech_recognition as sr
 API_KEY = "texE21-QTUFP"
 PROJECT_TOKEN = "tigYU5p1stUq"
 RUN_TOKEN = "tEesdam2LkbE"
@@ -26,7 +29,7 @@ class Data:
     def get_total_deaths(self):
         data = self.data['total']
         for content in data:
-            if content['name']== "Deaths:":
+            if content['name'] == "Deaths:":
                 return content['value']
 
         return "0"
@@ -38,5 +41,21 @@ class Data:
                 return content
         return "0"
 data = Data(API_KEY,PROJECT_TOKEN)
-#print(data.get_country_data('Poland'))
+def speak(text):
+    engine = pyttsx3.init()
+    engine.say(text)
+    engine.runAndWait()
+
+def get_audio():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        audio = r.listen(source)
+        said = ""
+        try:
+            said = r.recognize_google(audio)
+        except Exception as e:
+            print("Exception",str(e))
+    return said.lower()
+print(get_audio())
+
 
